1c67861c0257b31b6211d12e4e045e50b5887ccf
umarahh4u <umarahh4u@gmail.com>
2024-07-03 13:34:12 +0100

    update

--- app.js
+++ app.js
@@ -22,8 +22,11 @@ app.listen(port, () => {
 app.use((req, res, next) => {
   req.visitorIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-  const city = geoip.lookup(req.visitorIp);
+  next();
+});
 
+app.use((req, res, next) => {
+  const city = geoip.lookup(req.visitorIp);
   req.visitor_city = city.city;
 
   next();
@@ -34,15 +37,30 @@ app.get("/api/hello", (req, res) => {
   const visitorsName = req.query.visitors_name
     ? req.query.visitors_name
     : "Visitor";
-  const city = req.visitor_city;
-  res.status(200).json({
-    status: "success",
-    data: {
-      client_ip: visitors_ip, // The IP address of the requester
-      location: city, // The city of the requester
-      greeting: `Hello, ${visitorsName}!, the temperature is 11 degrees Celcius in ${city}`,
-    },
-  });
+  // Get geo data based on IP address
+  const geo = geoip.lookup(visitors_ip);
+
+  if (geo) {
+    const city = geo.city;
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        client_ip: visitors_ip, // The IP address of the requester
+        location: city, // The city of the requester
+        greeting: `Hello, ${visitorsName}!, the temperature is 11 degrees Celcius in ${city}`,
+      },
+    });
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: {
+        client_ip: visitors_ip, // The IP address of the requester
+        location: "Not valid", // The city of the requester
+        greeting: `Hello, ${visitorsName}!, the temperature is 11 degrees Celcius is none `,
+      },
+    });
+  }
 });
 
 app.use(middlewares.notFound);
